8945bacd4d140834fa8aa8bf7675863940de5ec4
hericson01 <hericson00@gmail.com>
2024-09-25 14:44:30 -0300

    adicionado presença acolhimento

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function toggleVisibility(event, elementId) {
     event.preventDefault();
     var elements = document.querySelectorAll('.extra-links');
-    elements.forEach(function(element) {
+    elements.forEach(function (element) {
         if (element.id === elementId) {
             if (element.classList.contains('show')) {
                 element.classList.remove('show');
             } else {
                 element.classList.add('show');
                 // Adiciona fechamento automático após 10 segundos
-                setTimeout(function() {
+                setTimeout(function () {
                     element.classList.remove('show');
                 }, 5000);
             }
@@ -18,14 +18,14 @@ function toggleVisibility(event, elementId) {
     });
 }
 
-document.getElementById('expand').addEventListener('click', function(event) {
+document.getElementById('expand').addEventListener('click', function (event) {
     toggleVisibility(event, 'extra-links');
 });
 
-document.getElementById('expand2').addEventListener('click', function(event) {
+document.getElementById('expand2').addEventListener('click', function (event) {
     toggleVisibility(event, 'extra-links2');
 });
 
-document.getElementById('expand3').addEventListener('click', function(event) {
+document.getElementById('expand3').addEventListener('click', function (event) {
     toggleVisibility(event, 'extra-links3');
 });
